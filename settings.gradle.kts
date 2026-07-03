746fa56c2ee7617e672e4ea4dddbb775d2dcde34
foddyy <foddyy@users.noreply.github.com>
2026-07-03 15:51:47 +0800

    fix: Add Aliyun mirrors, upgrade Gradle to 8.7, increase timeouts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,9 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
     }
 }
 dependencyResolutionManagement {
@@ -11,6 +14,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
     }
 }
 

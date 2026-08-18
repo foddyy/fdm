8ec51a029b212f6decbdd0fe9623a3cf9648ad4f
foddyy <foddyy@users.noreply.github.com>
2026-08-18 22:18:11 +0800

    fix: 将 aaptOptions 移到 android 块内

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,16 +29,16 @@ android {
             // 不添加后缀
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    
+
     kotlinOptions {
         jvmTarget = "17"
     }
-    
+
     buildFeatures {
         viewBinding = true
         dataBinding = true
